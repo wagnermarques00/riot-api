390b0fca92e3f30fa4c4ca8a5733a85074fda225
wagnermarques00 <wagnermarques00@gmail.com>
2024-10-27 15:21:27 -0300

    fix(versões): carregamento

--- riot-api/src/App.vue
+++ riot-api/src/App.vue
@@ -2,28 +2,30 @@
   <header>
     <NavBar :versao="versao"/>
   </header>
-
-  <RouterView />
+  <section v-if="carregado" class="pt-24 px-8 pb-7 bg-white dark:bg-gray-900 text-gray-500 dark:text-gray-400 min-h-screen antialiased">
+    <RouterView />
+  </section>
 </template>
 
 <script setup>
-import { onMounted, provide, ref } from 'vue'
+import { onBeforeMount, ref } from 'vue'
 import { RouterView } from 'vue-router'
 
 import ApiVersoes from './api/apiVersoes';
 import NavBar from './components/NavBar.vue';
 
 const versao = ref('')
+const carregado = ref(false)
 
 async function listarUltimaVersaoLoL() {
   const apiVersoes = new ApiVersoes()
   const versoes = await apiVersoes.listarVersoes()
   versao.value = versoes[0]
+  apiVersoes.atualizarVersao(versao.value)
 }
 
-onMounted(async () => {
-  listarUltimaVersaoLoL()
+onBeforeMount(async () => {
+  await listarUltimaVersaoLoL()
+  carregado.value = true
 })
-
-provide('versao', versao.value)
 </script>
