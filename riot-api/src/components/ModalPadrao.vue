861d83d14f0e40a740b638fbc1d478971810bf99
wagnermarques00 <wagnermarques00@gmail.com>
2024-10-27 19:56:55 -0300

    feat(modal padrão)

---
+++ riot-api/src/components/ModalPadrao.vue
@@ -0,0 +1,75 @@
+<template>
+  <div
+    v-if="exibirModal"
+    tabindex="-1"
+    class="flex overflow-y-auto overflow-x-hidden fixed top-0 bottom-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+    @click="fecharModal"
+  >
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+      <!-- Modal content -->
+      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <!-- Modal header -->
+        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+          <div>
+            <h3 class="text-xl font-semibold text-gray-900 dark:text-white" >
+              {{ props.titulo }}
+            </h3>
+            <h4 class="text-sm font-normal text-gray-900 dark:text-white" >
+              {{ props.subtitulo }}
+            </h4>
+          </div>
+          <button
+            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            title="fechar modal"
+            @click="fecharModal"
+          >
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+          </button>
+        </div>
+        <!-- Modal body -->
+        <div class="p-4 md:p-5 space-y-4">
+          <slot name="body" />
+        </div>
+        <!-- Modal footer -->
+        <div
+          v-if="$slots.footer"
+          class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600"
+        >
+          <slot name="footer"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const exibirModal = ref(true)
+
+const props = defineProps({
+  subtitulo: {
+    type: String,
+    default: ''
+  },
+  titulo: {
+    type: String,
+    default: ''
+  }
+})
+
+function abrirModal() {
+  exibirModal.value = true
+}
+
+function fecharModal() {
+  exibirModal.value = false
+}
+
+defineExpose({
+  abrirModal,
+  fecharModal
+})
+</script>
