98f55d01f997e72e81e739e695ed1e431c4ba9f6
wagnermarques00 <wagnermarques00@gmail.com>
2024-10-29 18:46:29 -0300

    Ajuste da rota

--- riot-api/src/router/index.js
+++ riot-api/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import CampeaoView from '../views/CampeaoView.vue'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/home/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
