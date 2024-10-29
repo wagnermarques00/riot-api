2e0e6097f760b84787df2f5ce988e516bd0fa38e
wagnermarques00 <wagnermarques00@gmail.com>
2024-10-29 20:31:05 -0300

    História do campeão

---
+++ riot-api/src/views/home/components/HistoriaCampeao.vue
@@ -0,0 +1,14 @@
+<template>
+  <p class="font-normal text-gray-700 dark:text-gray-400">
+    {{ props.historia }}
+  </p>
+</template>
+
+<script setup>
+const props = defineProps({
+  historia: {
+    type: String,
+    required: true
+  }
+})
+</script>
