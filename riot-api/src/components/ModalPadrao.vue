03905aff2b7c729f3c797bd8f98b846264eef780
wagnermarques00 <wagnermarques00@gmail.com>
2024-10-29 18:46:07 -0300

    Modal

--- riot-api/src/components/ModalPadrao.vue
+++ riot-api/src/components/ModalPadrao.vue
@@ -2,7 +2,7 @@
   <div
     v-if="exibirModal"
     tabindex="-1"
-    class="flex overflow-y-auto overflow-x-hidden fixed top-0 bottom-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+    class="flex overflow-y-auto overflow-x-hidden fixed top-0 bottom-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-gray-900/75"
     @click="fecharModal"
   >
     <div class="relative p-4 w-full max-w-2xl max-h-full">
@@ -10,13 +10,16 @@
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
         <!-- Modal header -->
         <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-          <div>
-            <h3 class="text-xl font-semibold text-gray-900 dark:text-white" >
-              {{ props.titulo }}
-            </h3>
-            <h4 class="text-sm font-normal text-gray-900 dark:text-white" >
-              {{ props.subtitulo }}
-            </h4>
+          <div class="flex justify-center">
+            <slot name="title" />
+            <div>
+              <h3 class="text-xl font-semibold text-gray-900 dark:text-white" >
+                {{ props.titulo }}
+              </h3>
+              <h4 class="text-sm font-normal text-gray-900 dark:text-white" >
+                {{ props.subtitulo }}
+              </h4>
+            </div>
           </div>
           <button
             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
@@ -47,8 +50,9 @@
 <script setup>
 import { ref } from 'vue'
 
-const exibirModal = ref(true)
+const exibirModal = ref(false)
 
+const emit = defineEmits(['fechar-modal'])
 const props = defineProps({
   subtitulo: {
     type: String,
@@ -66,6 +70,7 @@ function abrirModal() {
 
 function fecharModal() {
   exibirModal.value = false
+  emit('fechar-modal')
 }
 
 defineExpose({
