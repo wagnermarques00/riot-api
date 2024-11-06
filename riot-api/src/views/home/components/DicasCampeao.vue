ce14a7c72b710ececbd365b15907093e571cab1f
wagnermarques00 <wagnermarques00@gmail.com>
2024-11-06 08:50:01 -0300

    Aplicação do container

--- riot-api/src/views/home/components/DicasCampeao.vue
+++ riot-api/src/views/home/components/DicasCampeao.vue
@@ -1,28 +1,31 @@
 <template>
-  <div>
-    <p class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-      Dicas para os aliados:
-    </p>
-    <ul class="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
-      <li v-for="dica in props.dicasAliados" :key="dica">
-        {{ dica }}
-      </li>
-    </ul>
-  </div>
-  <div class="pt-4">
-    <p class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-      Dicas para os inimigos:
-    </p>
-    <ul class="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
-      <li v-for="dica in props.dicasInimigos" :key="dica">
-        {{ dica }}
-      </li>
-    </ul>
-  </div>
-
+  <ContainerTexto>
+    <div>
+      <p class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Dicas para os aliados:
+      </p>
+      <ul class="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+        <li v-for="dica in props.dicasAliados" :key="dica">
+          {{ dica }}
+        </li>
+      </ul>
+    </div>
+    <div class="pt-4">
+      <p class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Dicas para os inimigos:
+      </p>
+      <ul class="space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
+        <li v-for="dica in props.dicasInimigos" :key="dica">
+          {{ dica }}
+        </li>
+      </ul>
+    </div>
+  </ContainerTexto>
 </template>
 
 <script setup>
+import ContainerTexto from '@/components/ContainerTexto.vue';
+
 const props = defineProps({
   dicasAliados: {
     type: Array,
--- riot-api/src/views/home/components/HistoriaCampeao.vue
+++ riot-api/src/views/home/components/HistoriaCampeao.vue
@@ -1,10 +1,14 @@
 <template>
-  <p class="font-normal text-gray-700 dark:text-gray-400">
-    {{ props.historia }}
-  </p>
+  <ContainerTexto>
+    <p class="font-normal text-gray-700 dark:text-gray-400">
+      {{ props.historia }}
+    </p>
+  </ContainerTexto>
 </template>
 
 <script setup>
+import ContainerTexto from '@/components/ContainerTexto.vue';
+
 const props = defineProps({
   historia: {
     type: String,
