3287e0d5c5adcc5e4038303f1880afba210459d0
wagnermarques00 <wagnermarques00@gmail.com>
2024-11-08 19:36:32 -0300

    Ajustes no modal

--- riot-api/src/components/ModalPadrao.vue
+++ riot-api/src/components/ModalPadrao.vue
@@ -3,9 +3,8 @@
     v-if="exibirModal"
     tabindex="-1"
     class="flex overflow-y-auto overflow-x-hidden fixed top-0 bottom-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-gray-900/75"
-    @click="fecharModal"
   >
-    <div class="relative p-4 w-full max-w-2xl max-h-full">
+    <div class="relative p-4 w-full max-w-screen-lg max-h-full">
       <!-- Modal content -->
       <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
         <!-- Modal header -->
