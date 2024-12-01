d830432cff0d0d7c26fc6697f7457c86a56e12bd
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2024-12-01 11:38:39 -0300

    Removendo ;

--- riot-api/src/views/home/components/DicasCampeao.vue
+++ riot-api/src/views/home/components/DicasCampeao.vue
@@ -24,7 +24,7 @@
 </template>
 
 <script setup>
-import ContainerTexto from '@/components/ContainerTexto.vue';
+import ContainerTexto from '@/components/ContainerTexto.vue'
 
 const props = defineProps({
   dicasAliados: {
