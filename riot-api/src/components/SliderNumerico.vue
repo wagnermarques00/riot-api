13c3fbb90a767b1398c4c269f272886a309ff745
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2024-12-01 11:56:40 -0300

    Ajuste de tipagem

--- riot-api/src/components/SliderNumerico.vue
+++ riot-api/src/components/SliderNumerico.vue
@@ -3,7 +3,7 @@
     <label :for="`slider-numerico-${props.label}`" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ props.label }}: {{ valorSelecionado }}</label>
     <input
       :id="`slider-numerico-${props.label}`"
-      v-model="valorSelecionado"
+      v-model.number="valorSelecionado"
       class="w-full h-3 bg-gray-200 rounded-lg appearance-none cursor-pointer range-lg dark:bg-gray-700"
       type="range"
       :max="props.valores[props.valores.length -1]"
@@ -33,5 +33,5 @@ const props = defineProps({
   }
 })
 
-const valorSelecionado = defineModel('valorSelecionado', { type: [String, Object], default: '' })
+const valorSelecionado = defineModel('valorSelecionado', { type: [Number], default: 0 })
 </script>
