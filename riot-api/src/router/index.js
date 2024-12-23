67e406049c6bd4a177c3939a9a652658a223a28f
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2024-12-23 15:52:43 -0300

    Tela inicial dos ícones

--- riot-api/src/router/index.js
+++ riot-api/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/home/HomeView.vue'
+import IconesView from '../views/icones/IconesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'campeões',
       component: HomeView
+    },
+    {
+      path: '/icones',
+      name: 'ícones',
+      component: IconesView
     }
   ]
 })
---
+++ riot-api/src/views/icones/IconesView.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>Ícones</p>
+</template>
