20ac60fbaa46cde16a18c36bcb09c5ed89b7bb7d
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2025-03-16 16:31:13 -0300

    Removendo valores desnecessários

--- riot-api/src/views/campeoes/CampeoesView.vue
+++ riot-api/src/views/campeoes/CampeoesView.vue
@@ -1,11 +1,4 @@
 <template>
-  <div class="flex justify-left">
-    <DropDown
-      titulo="Papéis"
-      :opcoes="papeisTraduzidos"
-      @opcao-clicada="filtrarPapeis"
-    />
-  </div>
   <GridCampeoes @abrir-detalhes-campeao="idCampeaoSelecionado = $event" />
   <ModalCampeao
     :id-campeao-selecionado="idCampeaoSelecionado"
@@ -15,23 +8,8 @@
 
 <script setup>
 import { ref } from 'vue'
-import DropDown from '@/components/DropDown.vue'
 import GridCampeoes from './components/GridCampeoes.vue'
 import ModalCampeao from './components/ModalCampeao.vue'
 
-const papeis = [
-  { original: 'Assassin', traduzido: 'Assassino' },
-  { original: 'Marksman', traduzido: 'Atirador' },
-  { original: 'Fighter', traduzido: 'Lutador' },
-  { original: 'Mage', traduzido: 'Mago' },
-  { original: 'Support', traduzido: 'Suporte'},
-  { original: 'Tank', traduzido: 'Tanque' }
-]
-const papeisTraduzidos = papeis.map(papel => papel.traduzido)
-
 const idCampeaoSelecionado = ref('')
-
-function filtrarPapeis(value) {
-  alert(value)
-}
 </script>
