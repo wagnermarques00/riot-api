797dd4fa78313458afa6e71f37eb64a453f4e211
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2025-03-16 16:31:23 -0300

    Ajustes na view

--- riot-api/src/views/feiticos/FeiticosView.vue
+++ riot-api/src/views/feiticos/FeiticosView.vue
@@ -1,7 +1,15 @@
 <template>
-  <GridFeiticos />
+  <GridFeiticos @abrir-detalhes-feitico="idFeiticoSelecionado = $event"/>
+  <ModalFeitico
+    :id-feitico-selecionado="idFeiticoSelecionado"
+    @fechar-modal-feitico="idFeiticoSelecionado = ''"
+  />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import GridFeiticos from './components/GridFeiticos.vue'
+import ModalFeitico from './components/ModalFeitico.vue'
+
+const idFeiticoSelecionado = ref('')
 </script>
