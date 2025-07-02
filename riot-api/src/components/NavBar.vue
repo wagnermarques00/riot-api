102f1219d7e26d237316cb13c3d451a2bf5fbb23
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2025-07-02 15:36:46 -0300

    Remove Builds

--- riot-api/src/components/NavBar.vue
+++ riot-api/src/components/NavBar.vue
@@ -5,15 +5,6 @@
         <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
             <RouterLink
-              to="/builds"
-              class="block py-2 px-3 bg-blue-700 rounded md:bg-transparent  md:p-0"
-              :class="gerarClasseLink('/builds')"
-            >
-              Builds
-            </RouterLink>
-          </li>
-          <li>
-            <RouterLink
               to="/"
               class="block py-2 px-3 bg-blue-700 rounded md:bg-transparent  md:p-0"
               :class="gerarClasseLink('/')"
